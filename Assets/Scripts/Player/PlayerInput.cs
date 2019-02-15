8baf1a1df1abf5051df01414144f7e8b48476986
HenryBanks <harry_b_15@hotmail.co.uk>
2019-02-15 13:59:17 +0000

    Controller support

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -15,24 +15,24 @@ public class PlayerInput : MonoBehaviour {
 	}
 
 	public bool JumpInput(){
-		bool jumpRequest = Input.GetKey(KeyCode.Space);
+		bool jumpRequest = Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.JoystickButton16);
 		return jumpRequest;
 	}
 
     public bool JumpInputDown()
     {
-        bool jumpRequest = Input.GetKeyDown(KeyCode.Space);
+        bool jumpRequest = Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.JoystickButton16);
         return jumpRequest;
     }
 
     public bool JumpInputUp()
     {
-        bool jumpRequest = Input.GetKeyUp(KeyCode.Space);
+        bool jumpRequest = Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.JoystickButton16);
         return jumpRequest;
     }
 
     public bool DashInputDown(){
-		return Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.LeftShift);
+		return Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.JoystickButton14);
 	}
 
 
