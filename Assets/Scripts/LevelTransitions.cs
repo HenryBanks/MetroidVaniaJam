f9dee34bbeb6aadbf1e1d305dabad638973b83eb
HenryBanks <harry_b_15@hotmail.co.uk>
2019-02-26 22:32:35 +0000

    Basic level transitioning added, with options for where in level to spawn

---
+++ Assets/Scripts/LevelTransitions.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransitions : MonoBehaviour
+{
+    public static LevelTransitions instance;
+
+    int positionIndex=0;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            LoadLevelWithPosition(1, 1);
+        }
+    }
+
+    //private void OnLevelWasLoaded(int level)
+    //{
+    //    Debug.Log("Level Loaded");
+    //    Debug.Log(SpawnPositions.instance.spawnPositionsList[positionIndex]);
+    //    Player.instance.transform.position=SpawnPositions.instance.spawnPositionsList[positionIndex];
+    //    Debug.Log(Player.instance.transform.position);
+    //}
+
+    void OnEnable()
+    {
+        Debug.Log("OnEnable called");
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("Scene Loaded");
+        Debug.Log(SpawnPositions.instance.spawnPositionsList[positionIndex]);
+        Player.instance.transform.position = SpawnPositions.instance.spawnPositionsList[positionIndex];
+        Debug.Log(Player.instance.transform.position);
+    }
+
+    public void LoadLevelWithPosition(int sceneIndex, int posIndex)
+    {
+        positionIndex = posIndex;
+        SceneManager.LoadScene(sceneIndex);
+
+    }
+
+}
---
+++ Assets/Scripts/SpawnPositions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPositions : MonoBehaviour
+{
+    public List<Vector2> spawnPositionsList;
+
+    public static SpawnPositions instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+}
